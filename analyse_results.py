bbc20cab27b2b6e5d71cb2a3afb0187eebb3e2c0
Gianluca Micchi <gianluca.micchi@gmail.com>
2019-06-07 17:53:06 +0200

    Improve the visualization

--- analyse_results.py
+++ analyse_results.py
@@ -7,30 +7,7 @@ import matplotlib.pyplot as plt
 from config import TEST_TFRECORDS, BATCH_SIZE, TEST_STEPS, TEST_INDICES, FEATURES, NOTES, QUALITY, SYMBOL
 from load_data import create_tfrecords_dataset
 
-TEST_STEPS = 1
-
-test_data = create_tfrecords_dataset(TEST_TFRECORDS, BATCH_SIZE, shuffle_buffer=1)
-test_data_iter = test_data.make_one_shot_iterator()
-x, y = test_data_iter.get_next()
-
-
-def check_predictions(y_true, y_pred, index):
-    return np.argmax(y_true[index], axis=-1) == np.argmax(y_pred[index], axis=-1)
-
-
-model = load_model('conv_lstm.h5')
-model.summary()
-test_predict = []  # It will have shape: [pieces, features, (batch size, length of sonata, feature size)]
-for i in range(TEST_STEPS):
-    print(f"step {i + 1} out of {TEST_STEPS}")
-    temp = model.predict(test_data.skip(i), steps=1, verbose=False)
-    test_predict.append(temp)
-
-test_truth = []
-with tf.Session() as sess:
-    for i in range(TEST_STEPS):
-        data = sess.run(y)
-        test_truth.append(data)
+TEST_TFRECORDS = './data/test_sonata.tfrecords'
 
 circle_of_fifth_order = [8, 3, 10, 5, 0, 7, 2, 9, 4, 11, 6, 1]
 circle_of_fifth_order += [x + 12 for x in circle_of_fifth_order]
@@ -48,20 +25,58 @@ tick_labels = [
     NOTES,
     SYMBOL
 ]
-for i in range(TEST_STEPS):
+
+test_data = create_tfrecords_dataset(TEST_TFRECORDS, BATCH_SIZE, shuffle_buffer=1)
+test_data_iter = test_data.make_one_shot_iterator()
+x, y = test_data_iter.get_next()
+
+
+def check_predictions(y_true, y_pred, index):
+    return np.argmax(y_true[index], axis=-1) == np.argmax(y_pred[index], axis=-1)
+
+
+def visualize_data(mode='probabilities'):
+    if mode not in ['probabilities', 'predictions']:
+        raise ValueError('mode should be either probabilities or predictions')
+    cmap = sns.color_palette(['#000000', '#f7f7f7', '#d73027']) if mode == 'predictions' else 'RdBu_r'
+
     for j in range(7):
         a = test_predict[i][j][0] if j > 0 else test_predict[i][j][0][:, circle_of_fifth_order]
         b = test_truth[i][j][0] if j > 0 else test_truth[i][j][0][:, circle_of_fifth_order]
-        ax = sns.heatmap(b - a, cmap='RdBu_r', center=0, vmin=-1, vmax=1,
-                         xticklabels=tick_labels[j])
+        if mode == 'predictions':
+            a = (a == np.max(a, axis=-1, keepdims=True))
+        ax = sns.heatmap(b - a, cmap=cmap, center=0, vmin=-1, vmax=1, xticklabels=tick_labels[j])
         colorbar = ax.collections[0].colorbar
         colorbar.set_ticks([-1, 0, +1])
         colorbar.set_ticklabels(['False Neg', 'True', 'False Pos'])
 
         ax.set(xlabel=FEATURES[j], ylabel='time',
                title=f"Sonata {TEST_INDICES[i // 12]}, transposed {i % 12 - 6} semitones - {FEATURES[j]}")
+        figManager = plt.get_current_fig_manager()
+        figManager.window.showMaximized()
         plt.show()
 
+
+model = load_model('conv_lstm.h5')
+model.summary()
+
+# Retrieve the true labels
+test_truth = []
+with tf.Session() as sess:
+    for i in range(TEST_STEPS):
+        data = sess.run(y)
+        test_truth.append(data)
+
+# Predict new labels and view the difference
+test_predict = []  # It will have shape: [pieces, features, (batch size, length of sonata, feature size)]
+for i in range(TEST_STEPS):
+    print(f"step {i + 1} out of {TEST_STEPS}")
+    temp = model.predict(test_data.skip(i), steps=1, verbose=False)
+    test_predict.append(temp)
+
+    visualize_data()
+
+# Calculate accuracy etc.
 func_tp, symb_tp, total = 0, 0, 0
 cp = [[] for _ in range(7)]  # dimensions will be: features, sonatas, frames
 tp = np.zeros(7)
