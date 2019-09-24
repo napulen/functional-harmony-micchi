Solve a bug for masking.

load_data.py
@@ -18,13 +18,14 @@ def _parse_function(proto, n):
 
     parsed_features = tf.io.parse_single_example(proto, feature)
     piano_roll = tf.transpose(tf.reshape(parsed_features['piano_roll'], (n, -1)))
+    mask = tf.ones([tf.shape(parsed_features['label_key'])[0], 1], tf.bool)  # whatever label would work
     y_key = tf.one_hot(parsed_features['label_key'], depth=CLASSES_KEY)
     y_dg1 = tf.one_hot(parsed_features['label_degree_primary'], depth=CLASSES_DEGREE)
     y_dg2 = tf.one_hot(parsed_features['label_degree_secondary'], depth=CLASSES_DEGREE)
     y_qlt = tf.one_hot(parsed_features['label_quality'], depth=CLASSES_QUALITY)
     y_inv = tf.one_hot(parsed_features['label_inversion'], depth=CLASSES_INVERSION)
     y_roo = tf.one_hot(parsed_features['label_root'], depth=CLASSES_ROOT)
-    return piano_roll, tuple([y_key, y_dg1, y_dg2, y_qlt, y_inv, y_roo])
+    return (piano_roll, mask), tuple([y_key, y_dg1, y_dg2, y_qlt, y_inv, y_roo])
 
 
 def create_tfrecords_dataset(input_path, batch_size, shuffle_buffer, n):
@@ -40,13 +41,20 @@ def create_tfrecords_dataset(input_path, batch_size, shuffle_buffer, n):
     def _parse(proto):
         return _parse_function(proto, n)
 
-    padded_shapes = (tf.TensorShape([None, n]),
-                     (tf.TensorShape([None, CLASSES_KEY]),
-                     tf.TensorShape([None, CLASSES_DEGREE]),
-                     tf.TensorShape([None, CLASSES_DEGREE]),
-                     tf.TensorShape([None, CLASSES_QUALITY]),
-                     tf.TensorShape([None, CLASSES_INVERSION]),
-                     tf.TensorShape([None, CLASSES_ROOT]),))
+    padded_shapes = (
+        (
+            tf.TensorShape([None, n]),
+            tf.TensorShape([None, 1]),
+        ),
+        (
+            tf.TensorShape([None, CLASSES_KEY]),
+            tf.TensorShape([None, CLASSES_DEGREE]),
+            tf.TensorShape([None, CLASSES_DEGREE]),
+            tf.TensorShape([None, CLASSES_QUALITY]),
+            tf.TensorShape([None, CLASSES_INVERSION]),
+            tf.TensorShape([None, CLASSES_ROOT]),
+        )
+    )
 
     dataset = tf.data.TFRecordDataset(input_path).map(_parse, num_parallel_calls=16).shuffle(
         shuffle_buffer).repeat().padded_batch(batch_size, padded_shapes).prefetch(2)

model.py
@@ -68,16 +68,24 @@ def create_model(name, n, derive_root=False):
     :return:
     """
     notes = Input(shape=(None, n), name="piano_roll_input")
+    mask = Input(shape=(None, 1), name="mask_input")
     x = DenseNetLayer(notes, 4, 5, n=1)
     x = MaxPooling1D(2, 2, padding='same', data_format='channels_last')(x)
     x = DenseNetLayer(x, 4, 5, n=2)
     x = MaxPooling1D(2, 2, padding='same', data_format='channels_last')(x)
     x = DilatedConvLayer(x, 4, 64)  # total context: 3**4 = 81 eight notes, i.e., typically 5 measure before and after
-    x = Masking()(x)
+
+    # Super-ugly hack otherwise tensorflow can't save the model, see https://stackoverflow.com/a/55229794/5048010
+    def apply_mask(e):
+        return tf.multiply(e, mask)
+
+    x = Lambda(lambda e: apply_mask(e), name='apply_mask')(x)
+
+    x = Masking()(x)  # is this useless?
     # x = Bidirectional(GRU(64, return_sequences=True, dropout=0.3))(x)
     x = TimeDistributed(Dense(64, activation='tanh'))(x)
     y = MultiTaskLayer(x, derive_root)
-    model = Model(inputs=notes, outputs=y, name=name)
+    model = Model(inputs=[notes, mask], outputs=y, name=name)
     return model
 
 