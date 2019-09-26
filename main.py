f572d0f115b39a39b7fd831c8a907101eb8ed6a6
Gianluca <gianluca.micchi@gmail.com>
2019-09-26 14:39:55 +0200

    Introduce the model checkpoint callback.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 import seaborn as sns
 from matplotlib import pyplot as plt
 from tensorflow import enable_eager_execution
-from tensorflow.python.keras.callbacks import EarlyStopping, TensorBoard
+from tensorflow.python.keras.callbacks import EarlyStopping, TensorBoard, ModelCheckpoint
 
 from config import TRAIN_TFRECORDS, SHUFFLE_BUFFER, BATCH_SIZE, VALID_TFRECORDS, EPOCHS, STEPS_PER_EPOCH, \
     MODE, MODE2INPUT_SHAPE
@@ -23,25 +23,28 @@ def visualize_data(data):
     return
 
 
-def setup_paths(exploratory):
+def setup_paths(exploratory, type):
     if exploratory:
         enable_eager_execution()
         name = 'temp'
     else:
         i = 0
-        name = f'conv_dil_reduced_{MODE}_{i}'
+        name = '_'.join([type, MODE, i])
         while name in os.listdir('logs'):
             i += 1
-            name = f'conv_dil_reduced_{MODE}_{i}'
+            name = '_'.join([type, MODE, i])
 
     folder = os.path.join('logs', name)
     os.makedirs(folder)
+
     return folder, name
 
 
 timeout = None
+exploratory = False
 if __name__ == '__main__':
-    model_folder, model_name = setup_paths(exploratory=False)
+    model_folder, model_name = setup_paths(exploratory, type='conv_dil_reduced')
+    model_path = os.path.join(model_folder, model_name + '.h5')
     n = MODE2INPUT_SHAPE[MODE]
     train_data = create_tfrecords_dataset(TRAIN_TFRECORDS, BATCH_SIZE, SHUFFLE_BUFFER, n)
     valid_data = create_tfrecords_dataset(VALID_TFRECORDS, 7, 1, n)
@@ -54,9 +57,11 @@ if __name__ == '__main__':
     callbacks = [
         EarlyStopping(patience=3),
         TensorBoard(log_dir=model_folder),
+        ModelCheckpoint(filepath=model_path, save_best_only=True)
     ]
     if timeout is not None:
-        callbacks.append(TimeOut(t0=time.time(), timeout=timeout))
+        t0 = time.time()
+        callbacks.append(TimeOut(t0=t0, timeout=timeout))
 
     # weights = [1., 0.5, 1., 1., 0.5, 2.]  # [y_key, y_dg1, y_dg2, y_qlt, y_inv, y_roo]
     weights = [1., 1., 1., 1., 1., 1.]  # [y_key, y_dg1, y_dg2, y_qlt, y_inv, y_roo]
@@ -65,4 +70,4 @@ if __name__ == '__main__':
     model.fit(train_data, epochs=EPOCHS, steps_per_epoch=STEPS_PER_EPOCH, validation_data=valid_data,
               validation_steps=1, callbacks=callbacks)
 
-    model.save(os.path.join(model_folder, model_name + '.h5'))
+    # model.save(model_path)
