38c7dc84c816564cd1486d0cf4db5d6341c2176c
giamic <gianluca.micchi@gmail.com>
2022-03-04 16:12:49 +0000

    Comment out broken stuff

--- frog/analysis/dataset_analysis.py
+++ frog/analysis/dataset_analysis.py
@@ -131,143 +131,151 @@ def find_pitch_extremes(score_file):
 
 # FIXME: The rest of this module is currently broken. Throw it away?
 
-
-def calculate_transpositions():
-    """
-
-    :return:
-    """
-    nl_pitches, nr_pitches = calculate_lr_transpositions_pitches(piano_roll, spelling)
-    nl_keys, nr_keys = calculate_lr_transpositions_key(chords, spelling)
-    nl, nr = -min(nl_keys, nl_pitches), min(nr_keys, nr_pitches)
-    folders = [os.path.join(DATA_FOLDER, "train"), os.path.join(DATA_FOLDER, "valid")]
-
-    flatwards, sharpwards = [], []
-    for folder in folders:
-        chords_folder = os.path.join(folder, "chords")
-        scores_folder = os.path.join(folder, "scores")
-        file_names = [".".join(fn.split(".")[:-1]) for fn in os.listdir(chords_folder)]
-        for fn in file_names:
-            # if fn not in ['ncs_Chausson_Ernest_-_7_Melodies_Op.2_No.7_-_Le_Colibri']:
-            #     continue
-            print(fn)
-            cf = os.path.join(chords_folder, f"{fn}.csv")
-            sf = os.path.join(scores_folder, f"{fn}.mxl")
-            chord_labels = _load_chord_labels(cf)
-            piano_roll, nl_pitches, nr_pitches = load_score_spelling_bass(
-                sf, 8, False
-            )  # sharpwards in [1, 35]
-            nl_keys, nr_keys = calculate_lr_transpositions_key(
-                chord_labels, spelling
-            )  # sharpwards in [1, 35]
-            flatwards.append(min(nl_keys, nl_pitches))
-            sharpwards.append(min(nr_keys, nr_pitches))
-
-    print(f"flatward transpositions  : {sorted(Counter(flatwards).items())}")
-    print(f"sharpward transpositions : {sorted(Counter(sharpwards).items())}")
-    return flatwards, sharpwards
-
-
-def weighted_avg_and_std(values, weights):
-    """
-    Return the weighted average and standard deviation.
-
-    values, weights -- Numpy ndarrays with the same shape.
-    """
-    average = np.average(values, weights=weights)
-    # Fast and numerically precise:
-    variance = np.average((values - average) ** 2, weights=weights)
-    return average, math.sqrt(variance)
-
-
-def plot_flattest_sharpest(ff, ss):
-    sns.distplot(
-        ff, bins=np.arange(36) - 0.5, label="flattest note"
-    )  # need 36 boundaries for 35 bins
-    sns.distplot(ss, bins=np.arange(36) - 0.5, label="sharpest note")
-    max_occurrence = max(Counter(ff).most_common()[0][1], Counter(ss).most_common()[0][1])
-    max_ytick = max_occurrence - max_occurrence % 4
-    n_yticks = max_ytick // 4 + 1
-    plt.ylabel("occurrences")
-    plt.xticks(np.arange(35), PITCH_FIFTHS, rotation=90)
-    plt.yticks(
-        np.linspace(0, max_ytick, n_yticks) / len(ff),
-        [str(x) for x in np.linspace(0, max_ytick, n_yticks, dtype=int)],
-    )
-    plt.legend()
-    plt.savefig(os.path.join("../images", "flattest_sharpest.png"), bbox_inches="tight")
-    plt.show()
-    return
-
-
-def plot_range_histogram(r):
-    sns.distplot(r, bins=np.arange(36) - 0.5, label="range of notes")
-    max_occurrence = Counter(r).most_common()[0][1]
-    max_ytick = max_occurrence - max_occurrence % 4
-    n_yticks = max_ytick // 4 + 1
-    plt.xticks(np.arange(35), np.arange(35), rotation=90)
-    plt.yticks(
-        np.linspace(0, max_ytick, n_yticks) / len(ff),
-        [str(x) for x in np.linspace(0, max_ytick, n_yticks)],
-    )
-    plt.legend()
-    plt.savefig(os.path.join("../images", "range_histogram.png"), bbox_inches="tight")
-    plt.show()
-    return
-
-
-def plot_range_individual(ff, ss, r):
-    """
-    Experiments on how to plot individual ranges for all pieces.
-
-    :param ff:
-    :param ss:
-    :param r:
-    :return:
-    """
-    plt.style.use("ggplot")
-    cm = [(s + f) / 2 for s, f in zip(ss, ff)]
-    song_extremes = np.array([(x, y) for x, y in zip(ff, ss)], dtype=[("ff", "<i4"), ("ss", "<i4")])
-    song_cmr = np.array([(x, y) for x, y in zip(cm, r)], dtype=[("cm", "<i4"), ("r", "<i4")])
-    # order = np.argsort(song_extremes, order=('ff', 'ss'))
-    # order = np.argsort(song_cmr, order=('r', 'cm'))
-    order = np.argsort(song_cmr, order="cm")
-    cmap = sns.color_palette("BrBG", 26)
-    cm0 = min(cm)
-    for n, o in enumerate(order):
-        plt.hlines(y=n, linewidth=1.7, alpha=1.0, xmin=ff[o], xmax=ss[o])
-        # plt.hlines(y=n, color=cmap[int((cm[o] - cm0) * 2)], linewidth=2, alpha=1., xmin=ff[o]-cm[o], xmax=ss[o]-cm[o])
-    plt.scatter(ff[order], np.arange(len(order)))
-    plt.scatter(ss[order], np.arange(len(order)))
-    # plt.scatter([(s, y) for s, y in zip(ss[order], np.arange(len(order)))])
-    plt.savefig(os.path.join("../images", "range_individual.png"), bbox_inches="tight")
-    plt.show()
-    return
-
-
-def calculate_distribution_of_repetitions(r):
-    n = len(r)
-    rep = 35 - r
-    m_rep, s_rep = np.mean(rep), np.std(rep)
-    mat = np.zeros((n, n))
-    for i, x1 in enumerate(rep):
-        for j, x2 in enumerate(rep):
-            mat[i, j] = x1 / x2
-    flat = mat[~np.eye(n, dtype=bool)]  # this removes the diagonal and flattens at the same time
-    m_rel, s_rel = np.mean(flat), np.std(flat)
-
-    rs = sorted(Counter(rep).items())
-    res = np.zeros(len(rs))
-    n = np.sum([x[1] for x in rs])
-    for i in range(len(rs)):
-        temp = 0
-        for j in range(len(rs)):
-            weight = rs[j][1] / n
-            temp += (rs[i][0] / rs[j][0]) * weight
-        res[i] = temp
-    m_wgt, s_wgt = weighted_avg_and_std(res, [x[1] for x in rs])
-    # TODO: understand why s_rel != s_wgt while m_rel == m_wgt
-    return m_rel, s_rel, m_wgt, s_wgt
+#
+# def calculate_transpositions():
+#     """
+#
+#     :return:
+#     """
+#     nl_pitches, nr_pitches = calculate_lr_transpositions_pitches(piano_roll, spelling)
+#     nl_keys, nr_keys = calculate_lr_transpositions_key(chords, spelling)
+#     nl, nr = -min(nl_keys, nl_pitches), min(nr_keys, nr_pitches)
+#     folders = [os.path.join(DATA_FOLDER, "train"), os.path.join(DATA_FOLDER, "valid")]
+#
+#     flatwards, sharpwards = [], []
+#     for folder in folders:
+#         chords_folder = os.path.join(folder, "chords")
+#         scores_folder = os.path.join(folder, "scores")
+#         file_names = [".".join(fn.split(".")[:-1]) for fn in os.listdir(chords_folder)]
+#         for fn in file_names:
+#             # if fn not in ['ncs_Chausson_Ernest_-_7_Melodies_Op.2_No.7_-_Le_Colibri']:
+#             #     continue
+#             print(fn)
+#             cf = os.path.join(chords_folder, f"{fn}.csv")
+#             sf = os.path.join(scores_folder, f"{fn}.mxl")
+#             chord_labels = _load_chord_labels(cf)
+#             piano_roll, nl_pitches, nr_pitches = load_score_spelling_bass(
+#                 sf, 8, False
+#             )  # sharpwards in [1, 35]
+#             nl_keys, nr_keys = calculate_lr_transpositions_key(
+#                 chord_labels, spelling
+#             )  # sharpwards in [1, 35]
+#             flatwards.append(min(nl_keys, nl_pitches))
+#             sharpwards.append(min(nr_keys, nr_pitches))
+#
+#     print(f"flatward transpositions  : {sorted(Counter(flatwards).items())}")
+#     print(f"sharpward transpositions : {sorted(Counter(sharpwards).items())}")
+#     return flatwards, sharpwards
+#
+#
+# def weighted_avg_and_std(values, weights):
+#     """
+#     Return the weighted average and standard deviation.
+#
+#     values, weights -- Numpy ndarrays with the same shape.
+#     """
+#     average = np.average(values, weights=weights)
+#     # Fast and numerically precise:
+#     variance = np.average((values - average) ** 2, weights=weights)
+#     return average, math.sqrt(variance)
+#
+#
+# def plot_flattest_sharpest(ff, ss):
+#     sns.distplot(
+#         ff, bins=np.arange(36) - 0.5, label="flattest note"
+#     )  # need 36 boundaries for 35 bins
+#     sns.distplot(ss, bins=np.arange(36) - 0.5, label="sharpest note")
+#     max_occurrence = max(
+#         Counter(ff).most_common()[0][1], Counter(ss).most_common()[0][1]
+#     )
+#     max_ytick = max_occurrence - max_occurrence % 4
+#     n_yticks = max_ytick // 4 + 1
+#     plt.ylabel("occurrences")
+#     plt.xticks(np.arange(35), PITCH_FIFTHS, rotation=90)
+#     plt.yticks(
+#         np.linspace(0, max_ytick, n_yticks) / len(ff),
+#         [str(x) for x in np.linspace(0, max_ytick, n_yticks, dtype=int)],
+#     )
+#     plt.legend()
+#     plt.savefig(os.path.join("../images", "flattest_sharpest.png"), bbox_inches="tight")
+#     plt.show()
+#     return
+#
+#
+# def plot_range_histogram(r):
+#     sns.distplot(r, bins=np.arange(36) - 0.5, label="range of notes")
+#     max_occurrence = Counter(r).most_common()[0][1]
+#     max_ytick = max_occurrence - max_occurrence % 4
+#     n_yticks = max_ytick // 4 + 1
+#     plt.xticks(np.arange(35), np.arange(35), rotation=90)
+#     plt.yticks(
+#         np.linspace(0, max_ytick, n_yticks) / len(ff),
+#         [str(x) for x in np.linspace(0, max_ytick, n_yticks)],
+#     )
+#     plt.legend()
+#     plt.savefig(os.path.join("../images", "range_histogram.png"), bbox_inches="tight")
+#     plt.show()
+#     return
+#
+#
+# def plot_range_individual(ff, ss, r):
+#     """
+#     Experiments on how to plot individual ranges for all pieces.
+#
+#     :param ff:
+#     :param ss:
+#     :param r:
+#     :return:
+#     """
+#     plt.style.use("ggplot")
+#     cm = [(s + f) / 2 for s, f in zip(ss, ff)]
+#     song_extremes = np.array(
+#         [(x, y) for x, y in zip(ff, ss)], dtype=[("ff", "<i4"), ("ss", "<i4")]
+#     )
+#     song_cmr = np.array(
+#         [(x, y) for x, y in zip(cm, r)], dtype=[("cm", "<i4"), ("r", "<i4")]
+#     )
+#     # order = np.argsort(song_extremes, order=('ff', 'ss'))
+#     # order = np.argsort(song_cmr, order=('r', 'cm'))
+#     order = np.argsort(song_cmr, order="cm")
+#     cmap = sns.color_palette("BrBG", 26)
+#     cm0 = min(cm)
+#     for n, o in enumerate(order):
+#         plt.hlines(y=n, linewidth=1.7, alpha=1.0, xmin=ff[o], xmax=ss[o])
+#         # plt.hlines(y=n, color=cmap[int((cm[o] - cm0) * 2)], linewidth=2, alpha=1., xmin=ff[o]-cm[o], xmax=ss[o]-cm[o])
+#     plt.scatter(ff[order], np.arange(len(order)))
+#     plt.scatter(ss[order], np.arange(len(order)))
+#     # plt.scatter([(s, y) for s, y in zip(ss[order], np.arange(len(order)))])
+#     plt.savefig(os.path.join("../images", "range_individual.png"), bbox_inches="tight")
+#     plt.show()
+#     return
+#
+#
+# def calculate_distribution_of_repetitions(r):
+#     n = len(r)
+#     rep = 35 - r
+#     m_rep, s_rep = np.mean(rep), np.std(rep)
+#     mat = np.zeros((n, n))
+#     for i, x1 in enumerate(rep):
+#         for j, x2 in enumerate(rep):
+#             mat[i, j] = x1 / x2
+#     flat = mat[
+#         ~np.eye(n, dtype=bool)
+#     ]  # this removes the diagonal and flattens at the same time
+#     m_rel, s_rel = np.mean(flat), np.std(flat)
+#
+#     rs = sorted(Counter(rep).items())
+#     res = np.zeros(len(rs))
+#     n = np.sum([x[1] for x in rs])
+#     for i in range(len(rs)):
+#         temp = 0
+#         for j in range(len(rs)):
+#             weight = rs[j][1] / n
+#             temp += (rs[i][0] / rs[j][0]) * weight
+#         res[i] = temp
+#     m_wgt, s_wgt = weighted_avg_and_std(res, [x[1] for x in rs])
+#     # TODO: understand why s_rel != s_wgt while m_rel == m_wgt
+#     return m_rel, s_rel, m_wgt, s_wgt
 
 
 if __name__ == "__main__":
