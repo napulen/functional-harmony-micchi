d6b761b77af4a65fb0d80d2c77dd67672a87b985
Néstor Nápoles López <napulen@gmail.com>
2022-08-13 18:17:43 -0400

    Update runsomething.py

--- runsomething.py
+++ runsomething.py
@@ -1,8 +1,16 @@
 import subprocess
 import os
+import time
 
-for f in sorted(os.listdir("phd_testset")):
-    print(f)
-    subprocess.call(
-        f"python -m scripts.cra_run -p logs/ConvGruBlocknade_spelling_bass_2022-06-30_20-30-35/ phd_testset/{f} -o outputs/{f} -v".split()
-    )
+if __name__ == "__main__":
+    log = open("execution_times.log", "w")
+    for f in sorted(os.listdir("phd_testset")):
+        print(f)
+        start = time.time()
+        subprocess.call(
+            f"python -m scripts.cra_run -p logs/ConvGruBlocknade_spelling_bass_2022-06-30_20-30-35/ phd_testset/{f} -o outputs/{f} -v".split()
+        )
+        end = time.time()
+        log.write(f"{f}: {end - start:.2f}\n")
+        log.flush()
+    log.close()
